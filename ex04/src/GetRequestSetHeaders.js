c757d9a9582d2b0ecab998d1cc143df3b5cf7d84
hero-BA <suad.hero@gmail.com>
2021-04-02 10:17:26 +0200

    Modified GetRequestSetHeaders.js file

--- ex04/src/GetRequestSetHeaders.js
+++ ex04/src/GetRequestSetHeaders.js
@@ -9,8 +9,8 @@ class GetRequestSetHeaders extends React.Component {
   componentDidMount() {
     const headers = { "Content-Type": "application/json" };
     fetch("https://api.npms.io/v2/search?q=react", { headers })
-      .then((response) => response.json())
-      .then((data) => this.setState({ totalReactPackages: data.total }));
+      .then(response => response.json())
+      .then(data => this.setState({ totalReactPackages: data.total }));
   }
 
   render() {
